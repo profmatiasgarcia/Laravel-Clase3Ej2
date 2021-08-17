3227662b77f64ff2b6cfef33a2c23d811e70738c
Prof Matias Garcia <info@profmatiasgarcia.com.ar>
2021-08-17 13:42:32 -0300

    Initial Commit

---
+++ readme.txt
@@ -0,0 +1,17 @@
+Para poder utilizar el ejemplo debera:
+
+1) crear un nuevo proyecto Laravel
+	:~$ laravel new clase3-ej2
+	
+2) copiar el contenido de esta carpeta completo a la carpeta del nuevo 	proyecto recien creado. 
+	
+3) Al pegar las carpetas y archivos solicitara sobreescribir los mismo, lo que se confirmara.
+
+4) Generar la key
+    php artisan key:generate
+
+5) crear en MySQL o MariaDB una base de datos llamada ejcrud
+
+6) Ejecutar las migraciones en la carpeta del proyecto
+    php artisan migrate
+    
---
+++ resources/views/productos/layout.blade.php
@@ -0,0 +1,21 @@
+{{-- Copyright (C) 2019 Prof Matias Garcia para -http://www.profmatiasgarcia.com.ar- con licencia GNU GPL3. 
+Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los términos de la Licencia Pública General de GNU según es publicada por la Free Software Foundation, bien con la versión 3 de dicha Licencia o bien (según su elección) con cualquier versión posterior. Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA, incluso sin la garantía MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
+Debería haber recibido una copia de la Licencia Pública General junto con este programa. Si no ha sido así ingrese a -http://www.gnu.org/licenses/- --}}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ejemplo CRUD	</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <div class="card" style="margin-top: 20px;">
+        <div class="card-body">
+            @yield('content')
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
